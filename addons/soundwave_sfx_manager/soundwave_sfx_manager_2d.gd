fb892a245727db4ff899ce234908da34081ec5ff
joko26 <playerone99@outlook.de>
2026-03-20 21:06:24 +0100

    :sparkles: added 3d sfx manager

--- addons/soundwave_sfx_manager/soundwave_sfx_manager_2d.gd
+++ addons/soundwave_sfx_manager/soundwave_sfx_manager_2d.gd
@@ -1,13 +1,15 @@
 extends Node2D
 class_name  SoundwaveSfx2D
 
-@export var songs: Dictionary[String, AudioStream]
+@export var sfx: Dictionary[String, AudioStream]
 
-func play_song(song_name):
-    if songs[song_name]:
+func play_song(sfx_name):
+    if sfx[sfx_name]:
         var audio_player = AudioStreamPlayer2D.new()
         add_child(audio_player)
+        # configure new audio palyer
         audio_player.bus = "sfx"
-        audio_player.stream = songs[song_name]
+        # play
+        audio_player.stream = sfx[sfx_name]
         audio_player.play()
         audio_player.finished.connect(audio_player.queue_free)
